8a2eda96f946325be4c063c695c036f3546c8a59
frozenbrother <raymond@wegotthis.com.au>
2022-02-03 02:03:19 +1100

    pushing for it check why it's having issues

---
+++ data/schema.sql
@@ -0,0 +1,10 @@
+create table employees (
+	emp_no INT not null,
+	emp_title_id VARCHAR not null,
+	birth_date date not null,
+	first_name varchar not null,
+	last_name varchar not null,
+	sex varchar not null,
+	hire_date date not null,
+	primary key (emp no)
+);
